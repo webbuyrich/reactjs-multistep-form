5070236c6f9e83d8fcb421e27c810e6fdf58db0e
webbuyrich <webbuyrich@gmail.com>
2019-10-01 00:14:47 -0700

    added react-bootstrap, added bootstrap form, on submit render personalData Page

---
+++ src/components/FormPersonalData.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export class FormPersonalData extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FormPersonalData
--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,13 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Button, Form, Alert  } from 'react-bootstrap';
 
 export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        // values variable
+        const { values, handleChange } = this.props;
+        
         return (
-            <div>
-                <h1>Hello from user details</h1>
-            </div>
+            <Container>
+                <Alert variant="success">
+                    <Alert.Heading>Enter User Details</Alert.Heading>                    
+                </Alert>
+                <Form>
+                    <Form.Group controlId="firstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            onChange={this.handleChange} 
+                            defaultValue={values.firstName}
+                            placeholder="Enter first name" 
+                        />                        
+                    </Form.Group>
+                    <Form.Group controlId="lastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            onChange={this.handleChange}
+                            defaultValue={values.lastName}
+                            placeholder="Enter first name" 
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control 
+                            type="email" 
+                            placeholder="Enter your email" 
+                            onChange={this.handleChange}
+                            defaultValue={values.email}
+                        />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                    <Button 
+                        variant="primary" 
+                        type="submit"
+                        onClick={this.continue}
+                    >
+                        Submit
+                </Button>
+                </Form>
+            </Container>
+            
         )
     }
 }
+
 
 export default FormUserDetails
